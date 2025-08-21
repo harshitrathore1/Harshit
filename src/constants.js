ab06015136695c3c2947101953863f98fcb39156
harshit <harshitrathore8118@gmail.com>
2025-08-21 23:34:47 +0530

    first

---
+++ src/constants.js
@@ -0,0 +1,255 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+//import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+//import gsapLogo from './assets/tech_logo/gsap.png';
+//import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+//import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+//import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+//import netlifyLogo from './assets/tech_logo/netlify.png';
+//import vercelLogo from './assets/tech_logo/vercel.png';
+//import postgreLogo from './assets/tech_logo/postgre.png';
+//import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+//import acropolisLogo from './assets/company_logo/acropolisLogo.jpg';
+import edunetLogo from './assets/company_logo/Edunet-logo.png';
+import uoonsLogo from './assets/company_logo/uoons-logo.jpg';
+
+// Education Section Logo's
+import kolping_logo from './assets/education_logo/kolping_logo.png';
+import acropolisLogo from './assets/education_logo/acropolisLogo.jpg';
+//import acropolisLogo from './assets/education_logo/acropolisLogo2.jpg';
+
+// Project Section Logo's
+//import profile2 from './assets/work_logo/profile2.jpg';
+import project1 from './assets/work_logo/InsightCV.png';
+import project2 from './assets/work_logo/AICalc.jpeg';
+import project3 from './assets/work_logo/HealthMate.png';
+import project4 from './assets/work_logo/AISummarizer.png';
+import project5 from './assets/work_logo/Tidio-chatbot.png';
+
+// import profile2 from './assets/work_logo/profile2.jpg';
+// import profile2 from './assets/work_logo/profile2.jpg';
+// import profile2 from './assets/work_logo/profile2.jpg';
+// import profile2 from './assets/work_logo/profile2.jpg';
+// import profile2 from './assets/work_logo/profile2.jpg';
+// import profile2 from './assets/work_logo/profile2.jpg';
+// import profile2 from './assets/work_logo/profile2.jpg';
+// import profile2 from './assets/work_logo/profile2.jpg';
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      // { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      // { name: 'Vercel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: uoonsLogo,
+      role: "React Developer",
+      company: "Uoons Ecommerce Private Limited",
+      date: "May 2024 - June 2025 ",
+      desc: "I interned as a React Developer at Uoons Ecommerce Pvt. Ltd., where I built responsive web pages, optimized React components and API integrations to improve performance, and transformed Figma designs into functional interfaces. My contributions helped enhance user engagement and reduce UI bugs, improving the overall user experience.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Redux",
+        "Tailwind CSS",
+        "Git",
+        "Github",
+        "Figma"
+      ],
+    },
+    {
+      id: 1,
+      img: edunetLogo,
+      role: "AI & Cloud Trainee",
+      company: "Edunet Foundation",
+      date: "February 2024 - March 2024",
+      desc: "I completed an internship in AI & Cloud Computing with Edunet Foundation in collaboration with AICTE, where I worked extensively with IBM Cloud and its AI services. The training focused on building and deploying AI-powered chatbots using IBM Watson Assistant, exploring cloud deployment workflows, and understanding how cloud platforms support scalable AI solutions. This experience strengthened my skills in natural language processing (NLP), cloud-based application deployment, and integrating AI tools into real-world use cases.",
+      skills: [
+        "IBM Cloud",
+        "IBM Watson Assistant",
+        "Chatbot Development",
+        "Natural Language Processing",
+        "AI Services Integration",
+      ],
+    },
+    // {
+    //   id: 2,
+    //   img: edunetLogo,
+    //   role: "Frontend Intern",
+    //   company: "School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: acropolisLogo,
+      school: "Acropolis Institute of Technology & Research, Indore",
+      date: "2021 - 2025",
+      grade: "7.84 CGPA",
+      desc: "I have completed my Bachelor's degree in Computer Science and Engineering (AIML) from Acropolis Institute of Technology & Research, Indore. During my time at Acropolis, I have gained a strong foundation in programming, algorithms, machine learning, artificial intelligence, and cloud computing. I have studied courses such as Data Structures, DBMS, Operating Systems, Computer Networks, and AI/ML. I actively participated in coding competitions, hackathons, and departmental activities, which helped enhance my problem-solving and teamwork skills. My experience at Acropolis has been crucial in shaping my technical abilities and professional growth.",
+      degree: "Bachelor of Technology - B.Tech (CSE - AIML)",
+    },
+    {
+      id: 1,
+      img: kolping_logo,
+      school: "Kolping Convent Higher Secondary School, Maksi",
+      date: "2020 - 2021",
+      grade: "92.2%",
+      desc: "I have completed my Senior Secondary education (12th Grade) from Kolping Convent Higher Secondary School, Maksi, with 92.2%. During this time, I focused on Mathematics and Science, which helped me build strong analytical and logical reasoning skills. My academic performance and active participation in school activities laid the foundation for my interest in computer science and problem-solving.",
+      degree: "Senior Secondary (Class XII)",
+    },
+    {
+      id: 2,
+      img: kolping_logo,
+      school: "Kolping Convent Higher Secondary School, Maksi",
+      date: "2018 - 2019",
+      grade: "90.6%",
+      desc: "I have completed my High School education (10th Grade) from Kolping Convent Higher Secondary School, Maksi, with 90.6%. My schooling years helped me develop discipline, consistency, and curiosity to learn, which continue to reflect in my academic and professional journey.",
+      degree: "High School (Class X)",
+    },
+    
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "InsightCV",
+      description:
+        "A modern resume and job application tracking platform built with React, TypeScript, and Tailwind CSS. InsightCV allows users to upload resumes, receive AI-powered feedback and ATS-friendly ratings using Claude, and track multiple job applications in one place. It leverages Puter.js for seamless serverless backend operations, React Router for navigation, and Zustand for efficient state management—delivering a smooth and scalable user experience.",
+      image: project1,
+      tags: ["React.js", "TypeScript", "Tailwind CSS", "React Router", "Zustand", "Puter.js", "Claude AI"],
+      github: "https://github.com/harshitrathore1/InsightCV",
+      webapp: "https://puter.com/app/insightcv",
+    },
+    {
+      id: 1,
+      title: "VisualMathGenius",
+      description:
+         "An AI-powered gesture-based math solver that allows users to solve mathematical problems using hand gestures via a webcam. It uses OpenCV and Google's Gemini AI to interpret handwritten-style gestures and provide step-by-step solutions. Built with Python, Django, and CVZone, the project offers a unique hands-free interactive experience for solving math problems.",
+      image: project2,
+      tags: ["Python", "OpenCV", "Django", "CVZone", "Gemini AI"],
+      github: "https://github.com/harshitrathore1/VisualMathGenius",
+      webapp: "",
+    },
+    {
+      id: 2,
+      title: "HealthMate",
+      description:
+        "HealthMate is an AI-powered medical chatbot designed to provide users with basic healthcare information and guidance. Built with a simple frontend using HTML, CSS, and JavaScript, and a Flask backend in Python, the chatbot leverages NLP to understand user queries and deliver accurate responses. It offers a clean and intuitive interface, making healthcare support more accessible.",
+      image: project3,
+      tags: ["HTML", "CSS", "JavaScript", "Python", "Flask", "NLP"],
+      github: "https://github.com/harshitrathore1/HealthMate",
+      webapp: "",
+    },
+    {
+      id: 3,
+      title: "AI Summarizer",
+      description:
+        "A Chrome extension that uses the Gemini API to generate AI-powered summaries of any webpage in three formats: Detailed, Brief, and Bullet Points. Built with JavaScript and Chrome Extension APIs, it enhances browsing productivity by providing instant content understanding directly within the browser.",
+      image: project4,
+      tags: ["HTML", "CSS", "JavaScript", "Gemini API"],
+      github: "https://github.com/harshitrathore1",
+      webapp: "",
+    },
+    
+    {
+      id: 4,
+      title: "Tidio AI Chatbot",
+      description:"An AI-powered customer support chatbot developed using Tidio. Designed to handle FAQs, automate responses, and improve customer engagement with real-time interaction. The bot integrates with website workflows and can be customized for different business needs, providing both efficiency and a seamless user experience.",
+      image: project5,
+      tags: ["Tidio", "Chatbot", "Automation", "AI"],
+      github: "https://github.com/harshitrathore1",
+      webapp: "",
+    },
+  ];  
